ad9007ef0609dc5cac124f0fba90213b96b21322
Thomas <thliang01@gmail.com>
2023-12-22 15:06:53 +0800

    Refactor abilities and powers visualization code

--- pages/third_abilities_powers.py
+++ pages/third_abilities_powers.py
@@ -2,18 +2,18 @@
 This module is used for visualizing
 the abilities and powers data in the Marvel Universe: Civil War data.
 """
+# Import libraries
 import streamlit as st
 import pandas as pd
 import plotly.express as px  # type: ignore
 
+# Read the data
 stats = pd.read_csv("data/charcters_stats.csv")
 
 st.title("Abilities & Powers")
 st.markdown("### Plotting from the raw stats data")
 st.dataframe(stats)
 
-# Filter the data
-# filtered_stats = stats.iloc[-226].query('Alignment != ""')
 
 # Reshape the data
 melted_stats = pd.melt(
@@ -24,15 +24,12 @@ melted_stats = pd.melt(
 )
 melted_stats = melted_stats[melted_stats["Key"] != "Total"]
 
-# st.dataframe(melted_stats)
-
 # Modify the Alignment column
 melted_stats["Alignment"] = melted_stats["Alignment"].str.title()
 
 # Group and arrange the data
 grouped_stats = melted_stats.groupby("Key").apply(lambda x: x.sort_values("Value", ascending=False))
 
-# st.dataframe(grouped_stats)
 
 # Plotting
 fig = px.strip(
@@ -59,3 +56,57 @@ fig.update_yaxes(title_text="Power")
 # Display the plot
 # fig.show()
 st.plotly_chart(fig)
+
+# Create Code Block
+code_block = """
+# Import libraries
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+# Read the data
+stats = pd.read_csv("data/charcters_stats.csv")
+
+# Reshape the data
+melted_stats = pd.melt(
+    stats,
+    id_vars=["Alignment", "Name"],
+    var_name="Key",
+    value_name="Value",
+)
+melted_stats = melted_stats[melted_stats["Key"] != "Total"]
+
+# Modify the Alignment column
+melted_stats["Alignment"] = melted_stats["Alignment"].str.title()
+
+# Group and arrange the data
+grouped_stats = melted_stats.groupby("Key").apply(lambda x: x.sort_values("Value", ascending=False))
+
+# Plotting
+fig = px.strip(
+    grouped_stats,
+    x="Key",
+    y="Value",
+    color="Key",
+    facet_col="Alignment",
+    facet_col_wrap=1,
+    title="Abilities & Powers",
+)
+
+# Update the layout
+fig.update_layout(
+    legend_title="Abilities & Powers",
+    xaxis={"tickfont": {"color": "white"}},
+    yaxis={"tickfont": {}},
+    title={"font": {"color": "seagreen"}},
+    font={"color": "white"},
+)
+fig.update_xaxes(title_text="Abilities")
+fig.update_yaxes(title_text="Power")
+
+# Display the plot
+# fig.show()
+st.plotly_chart(fig)
+"""
+
+st.code(code_block, language="python")
