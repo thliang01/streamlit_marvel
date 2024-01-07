627cbab50922e04e44f5c6b5470055544db891e8
Thomas <thliang01@gmail.com>
2024-01-07 21:33:25 +0800

    Refactor code to plot number of first appearances by year

--- pages/1 - first_appearance.py
+++ pages/1 - first_appearance.py
@@ -18,18 +18,21 @@ wiki = pd.read_csv('data/marvel-wikia-data.csv')
 
 st.dataframe(wiki)
 
+# Group by 'Year' and count the number of appearances
+year_counts = wiki['Year'].value_counts().sort_index()
+
+# Create an area chart
+plt.figure(figsize=(10, 6))
+plt.fill_between(year_counts.index, year_counts, color='skyblue', alpha=0.4)
+plt.plot(year_counts.index, year_counts, color='Slateblue', alpha=0.6)
+
+plt.title('Number of First Appearances by Year')
+plt.xlabel('Year')
+plt.ylabel('Number of First Appearances')
+
+# Display the plot in Streamlit
 st.markdown('### Plotting the number of first appearance by year')
-
-# Group by Year and count
-chart_data = wiki.groupby('Year').size().reset_index(name='n')
-
-# Create the plot
-fig, ax = plt.subplots(figsize=(10, 6))
-ax.fill_between(chart_data['Year'], chart_data['n'], color='royalblue', alpha=0.8)
-ax.plot(chart_data['Year'], chart_data['n'], color='red', linestyle='--')
-
-# Show the plot
-st.pyplot(fig)
+st.pyplot(plt)
 
 # Create Code Block
 CODE_BLOCK = """
@@ -41,17 +44,24 @@ import matplotlib.pyplot as plt
 # Read the data
 wiki = pd.read_csv("data/marvel-wikia-data.csv")
 
-# Group by Year and count
-chart_data = wiki.groupby("Year").size().reset_index(name="n")
+# Group by 'Year' and count the number of appearances
+year_counts = wiki['Year'].value_counts().sort_index()
 
-# Create the plot
-plt.figure(figsize=(10, 6))
-plt.fill_between(chart_data["Year"], chart_data["n"], color="royalblue", alpha=0.8)
-plt.plot(chart_data["Year"], chart_data["n"], color="red", linestyle="--")
+# Create an area chart
+plt.figure(figsize=(10,6))
+plt.fill_between(year_counts.index, year_counts, color="skyblue", alpha=0.4)
+plt.plot(year_counts.index, year_counts, color="Slateblue", alpha=0.6)
+
+plt.title('Number of First Appearances by Year')
+plt.xlabel('Year')
+plt.ylabel('Number of First Appearances')
+
+# Display the plot in Streamlit
+st.pyplot(plt)
 """
 
 st.code(CODE_BLOCK, language='python')
 
 st.markdown('## Tableau Version')
 
-st.image('images/area and trend chart of _Year of Appearance_.png')
+st.image('images/area and trend chart of _First Appearance_.png')
